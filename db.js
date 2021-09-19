ecbc072351b7681979fadd3c12c8cdc42b46d05c
Amarjit yadav <yadavamarjit772@gmail.com>
2021-09-20 00:08:09 +0530

    initial commit

--- db.js
+++ db.js
@@ -1,5 +1,70 @@
+const AWS = require('aws-sdk')
 require("dotenv").config()
 const id=process.env.AWS_ID
 const secret=process.env.AWS_SECRET
 const region=process.env.AWS_DEFAULT_REGION
 console.log(id,region,secret)
+
+
+// Aws congfiguration
+AWS.config.update({
+    region:region,
+    accessKeyId:id,
+    secretAccessKey:secret
+})
+
+
+const dynamoClient = new AWS.DynamoDB.DocumentClient()
+
+// declaring assigning table name in varable Table_Name
+const Table_Name = 'image-api'
+
+// function for providing all products
+const getProducts =async ()=>{
+    const params ={
+        TableName:Table_Name
+    }
+    const product =await dynamoClient.scan(params).promise()
+    console.log(product)
+    return product
+}
+getProducts()
+// function for adding product
+
+const addProduct = async (products)=>{
+    const params = {
+        TableName:Table_Name,
+        Item:products
+    }
+    return await dynamoClient.put(params).promise()
+}
+
+// function for getting single product
+const getProductbyId = async (id)=>{
+    const params = {
+        TableName:Table_Name,
+        Key:{
+            id
+        }
+    }
+    return await dynamoClient.get(params).promise()
+}
+
+// function for deleting product
+const deleteProductbyId = async (id)=>{
+    const params = {
+        TableName:Table_Name,
+        Key:{
+            id
+        }
+    }
+    return await dynamoClient.delete(params).promise()
+}
+
+module.exports = {
+    dynamoClient,
+    getProducts,
+    getProductbyId,
+    deleteProductbyId,
+    addProduct
+}
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express= require('express')
 const app=express()
 require('dotenv').config()
+const {getProducts} =require('./db')
 const port = process.env.PORT || 3000
 console.log(process.env.PORT)
 
 app.get('/',(req,res)=>{
     res.send("Working now")
 })
-app.get('/products',(req,res)=>{
-    res.send("product page")
+app.get('/products',async(req,res)=>{
+    try{
+        const product =await getProducts()
+        res.json(product)
+    }
+    catch (error){
+        console.log(err)
+        res.status(500).json({err:"Something went wrong"})
+    }
 })
 
 app.listen(port,()=>{
